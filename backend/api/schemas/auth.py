2f444c2d4f26b5e4d4bb35d2e1ea8fa384937599
Lior Singer <liorzinger123@gmail.com>
2026-06-07 20:37:22 +0300

    add register check for data

--- backend/api/schemas/auth.py
+++ backend/api/schemas/auth.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class Credentials(BaseModel):
@@ -7,6 +7,14 @@ class Credentials(BaseModel):
     
 
 class RegisterPayload(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., min_length=3)
     email: EmailStr
+    password: str = Field(..., min_length=8)
+
+    @field_validator('password')
+    def password_must_contain_rules(cls, v):
+        if not any(c.isupper() for c in v):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not any(c.isdigit() for c in v):
+            raise ValueError('Password must contain at least one number')
+        return v
