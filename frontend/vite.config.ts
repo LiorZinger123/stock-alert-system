aaef3b66a5537bf08984916a89f7b0281fa8f5e9
Lior Singer <liorzinger123@gmail.com>
2026-06-23 01:13:05 +0300

    fix build size warning

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
